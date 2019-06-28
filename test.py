dcc6d192f6395caceb1c97d49b67d4a7df1a6a0f
gulouguawen <13207690135@163.com>
2019-06-28 16:35:30 +0800

    updates

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-# @Project Name：machine-learning-project
+# @Project：machine-learning-project
 # @Author：shiyao
 # @Email：13207690135@163.com
 # @Time：2019-06-28 15:50:22
 # @IDE：PyCharm
 # @File Name：test.py
 
+# module test
+import matplotlib
+import numpy
+import pandas
+import sklearn
