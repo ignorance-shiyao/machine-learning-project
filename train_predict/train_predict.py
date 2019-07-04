941ca157528a49d0ef5cebfd5aa5e57075aba5f0
shiyao19951113 <820591937@qq.com>
2019-07-04 13:55:29 +0800

    updates

--- train_predict/train_predict.py
+++ train_predict/train_predict.py
@@ -18,29 +18,38 @@ from sklearn.linear_model import LinearRegression
 import sys
 import numpy
 
+# 输出全部数据
 numpy.set_printoptions(threshold=sys.maxsize)
 
 
+# 连接数据库
 def get_connection():
     connection = pymysql.connect(
         user='root',
         password='shiyao'
     )
-
     return connection
 
 
+# 读取训练集数据
 sql = 'select * from db_ml.train_data'
 
 train = pd.read_sql(sql=sql, con=get_connection())
 # print(train)
+
+# 读取 order_count 之外的所有列
 x = train.drop(labels='order_count', axis=1)
+
+# 读取 order_count
 y = train[['order_count']]
 # print(x,y)
 
+
+# 采用线性模型
 lr = LinearRegression()
 lr.fit(x, y)
 
+# 读取测试集数据
 sql = 'select * from db_ml.test_data'
 
 test = pd.read_sql(sql=sql, con=get_connection())
@@ -56,7 +65,13 @@ x_test = test[
     ]
 ]
 
+# 预测 order_count
 y_predict = lr.predict(x_test)
+# 将预测值存入model.csv
+with open('../output/model.csv', 'w') as f:
+    for i in y_predict:
+        for j in i:
+            f.write(str(j) + "\n")
 
 print('y predict', y_predict)
 
